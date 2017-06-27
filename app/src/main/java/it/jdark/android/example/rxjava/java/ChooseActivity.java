a519f9cda34aaa05be66d9b2b3a822fb49f3ba2b
jD4rk <jdm0b1l3@gmail.com>
2017-06-27 12:53:34 +0100

    switch to "shortcuts" (api > 25) instead productFlavours
    Signed-off-by: jD4rk <jdm0b1l3@gmail.com>

---
+++ app/src/main/java/it/jdark/android/example/rxjava/java/ChooseActivity.java
@@ -0,0 +1,17 @@
+package it.jdark.android.example.rxjava.java;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import it.jdark.android.example.rxjava.R;
+
+
+public class ChooseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose);
+    }
+
+}
